c0d9710571f2deb2beca81c6fd096d33f535c48e
mateledge <taniguchi.y@mateledge.co.jp>
2025-05-23 09:57:12 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 20 }}>
       <h1>表示成功</h1>
-      <p>この画面が表示されれば index.tsx が正常に機能しています！</p>
+      <p>この画面が出れば構成は100％成功です！</p>
     </div>
   );
 }
